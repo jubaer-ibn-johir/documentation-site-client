4de515fe420d8a0aaaffc0eb770b1fd7b549396c
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-07-24 01:22:52 +0600

    added navbar

---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { IoIosArrowDown } from 'react-icons/io'
+
+const Navbar = ({ children }) => {
+    return (
+        <div className="drawer drawer-end">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+                <div className="w-full navbar bg-white fixed top-0 shadow-lg lg:px-10">
+                    <div className="flex-1 px-2 mx-2 text-2xl font-bold"><Link to="/"><span className='text-primary text-3xl font-bold'>E</span>asy<span className='text-secondary text-3xl font-bold'>D</span>ocs</Link></div>
+                    <div className="flex-none lg:hidden">
+                        <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </label>
+                    </div>
+                    <div className="flex-none hidden lg:block">
+                        <ul className="menu menu-horizontal">
+                            <div className="menu menu-horizontal mr-[160px]">
+                                <li><Link className='hover:bg-white hover:text-primary' to="/docs">Docs</Link></li>
+                                <li className="dropdown dropdown-hover">
+                                    <label tabIndex="0" className='flex items-center hover:bg-white hover:text-primary'>Blogs<IoIosArrowDown></IoIosArrowDown></label>
+                                    <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                        <li><Link className='hover:bg-white hover:text-primary' to="/blog">Blog</Link></li>
+                                        <li><Link className='hover:bg-white hover:text-primary' to="/tutorial">Tutorial</Link></li>
+                                    </ul>
+                                </li>
+                                <li><Link className='hover:bg-white hover:text-primary' to="/dashboard">Dashboard</Link></li>
+                                <li className="dropdown dropdown-hover">
+                                    <label tabIndex="0" className='flex items-center hover:bg-white hover:text-primary'>Contact us<IoIosArrowDown></IoIosArrowDown></label>
+                                    <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                        <li><Link className='hover:bg-white hover:text-primary' to="/contact">Contact Us</Link></li>
+                                        <li><Link className='hover:bg-white hover:text-primary' to="/about">About</Link></li>
+                                    </ul>
+                                </li>
+                                <li><Link className='hover:bg-white hover:text-primary' to="/languages">Languages</Link></li>
+                            </div>
+                            <label className="swap swap-rotate mr-[20px]">
+                                <input type="checkbox" />
+                                <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
+                                <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
+
+                            </label>
+                            <li><Link className="btn bg-transparent text-primary btn-primary hover:text-white rounded-full" to="/courses">Our Courses</Link></li>
+                            <li><Link className='btn btn-primary text-white rounded-md ml-4' to="/signin">Sign In</Link></li>
+                        </ul>
+                    </div>
+                </div>
+                {children}
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+                <ul className="menu p-4 overflow-y-auto w-80 bg-white">
+                    <div className='text-2xl font-bold ml-4 flex items-center justify-between'>
+                        <Link to="/"><span className='text-primary text-3xl font-bold'>E</span>asy<span className='text-secondary text-3xl font-bold'>D</span>ocs</Link>
+                        <label className="swap swap-rotate mr-4 mt-2">
+                            <input type="checkbox" />
+                            <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
+                            <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
+
+                        </label>
+                    </div>
+                    <li><Link className='hover:bg-white hover:text-primary' to="/docs">Docs</Link></li>
+                    <div className="dropdown ml-4">
+                        <label tabIndex="0" className='flex items-center hover:bg-white hover:text-primary'>Blogs<IoIosArrowDown></IoIosArrowDown></label>
+                        <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><Link className='hover:bg-white hover:text-primary' to="/blog">Blog</Link></li>
+                            <li><Link className='hover:bg-white hover:text-primary' to="/tutorial">Tutorial</Link></li>
+                        </ul>
+                    </div>
+                    <li><Link className='hover:bg-white hover:text-primary' to="/dashboard">Dashboard</Link></li>
+                    <div className="dropdown ml-4">
+                        <label tabIndex="0" className='flex items-center hover:bg-white hover:text-primary'>Contact us<IoIosArrowDown></IoIosArrowDown></label>
+                        <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><Link className='hover:bg-white hover:text-primary' to="/contact">Contact Us</Link></li>
+                            <li><Link className='hover:bg-white hover:text-primary' to="/about">About</Link></li>
+                        </ul>
+                    </div>
+                    <li><Link className='hover:bg-white hover:text-primary' to="/languages">Languages</Link></li>
+                    <li><Link className="btn bg-transparent text-primary mb-3 ml-4 w-[150px] btn-primary hover:text-white rounded-full" to="/courses">Our Courses</Link></li>
+                    <li><Link className='btn btn-primary w-[150px] text-white rounded-md ml-4' to="/signin">Sign In</Link></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+export default Navbar;
