0023cd42209781653850f5b55495574fbba59b98
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-08-03 19:48:01 +0600

    Redux Documentation designed & Written

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/about/About';
 import Login from './components/autentication/Login';
 import SingUp from './components/autentication/SingUp';
 import ContactUs from './components/contactUs/ContactUs';
@@ -9,7 +10,11 @@ import NextDocs from './components/Docs/NextJS/NextDocs';
 import NextGetStarted from './components/Docs/NextJS/NextGetStarted';
 import Nextjs from './components/Docs/NextJS/Nextjs';
 import NextPages from './components/Docs/NextJS/NextPages';
+import WhyRedux from './components/Docs/Redux/WhyRedux';
 import Reactjs from './components/Docs/ReactJS/Reactjs';
+import GetStarted from './components/Docs/Redux/GetStarted';
+import Installation from './components/Docs/Redux/Installation';
+import ReduxDocs from './components/Docs/Redux/ReduxDocs';
 import Reduxjs from './components/Docs/Redux/Reduxjs';
 import Home from './components/home/Home';
 
@@ -17,6 +22,7 @@ import Footer from './components/shared/Footer';
 import Hearder from './components/shared/Hearder';
 import Navbar from './components/shared/Navbar';
 import NotFound from './components/shared/NotFound';
+import CoreConcepts from './components/Docs/Redux/CoreConcepts';
 
 function App() {
   return (
@@ -27,6 +33,7 @@ function App() {
         <Route path="/" element={<Home></Home>} />
         <Route path="/singup" element={<SingUp></SingUp>} />
         <Route path="/contact" element={<ContactUs></ContactUs>} />
+        <Route path="/about" element={<About></About>}></Route>
         <Route path="/docs" element={<Docs></Docs>} />
         <Route path="/reactjs" element={<Reactjs></Reactjs>} />
         <Route path="/redux" element={<Reduxjs></Reduxjs>} />
@@ -36,6 +43,12 @@ function App() {
           <Route index element={<NextGetStarted></NextGetStarted>}></Route>
           <Route path='next-pages' element={<NextPages></NextPages>}></Route>
           <Route path='getServerSideProps' element={<GetServerSideProps></GetServerSideProps>}></Route>
+        </Route>
+        <Route path='get-started' element={<ReduxDocs></ReduxDocs>}>
+          <Route index element={<GetStarted></GetStarted>}></Route>
+          <Route path='installation' element={<Installation></Installation>}></Route>
+          <Route path='why-redux' element={<WhyRedux></WhyRedux>}></Route>
+          <Route path='core-concepts' element={<CoreConcepts></CoreConcepts>}></Route>
         </Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
--- src/components/Docs/ReactJS/Reactjs.js
+++ src/components/Docs/ReactJS/Reactjs.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Reactjs = () => {
     return (
-        <div>
-        </div>
+        <div></div >
     );
 };
 
