057720af186597dce95b7a9958e32def2ab5236a
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-08-07 01:23:34 +0600

    Manage tutorial page & Add Tutorial form design done

---
+++ src/components/Dashboard/TutorialsManage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TutorialsManage = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">Manage Tutorials</h2>
+        </div>
+    );
+};
+
+export default TutorialsManage;
