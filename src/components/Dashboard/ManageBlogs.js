d53e2446f1dae0a070dccc0b6fac94d68b92e20a
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-08-06 22:55:34 +0600

    Blogs manage designed & README.md file updated

--- src/components/Dashboard/ManageBlogs.js
+++ src/components/Dashboard/ManageBlogs.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import { AiFillDelete } from 'react-icons/ai';
 
 const ManageBlogs = () => {
     return (
-        <div>
-            <h2 className='text-5xl'>Manage Blogs</h2>
+        <div className='lg:p-8 md:p-8 p-4'>
+            <div class="overflow-x-auto shadow-xl">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th style={{ "backgroundColor": "#82cbe5" }}></th>
+                            <th style={{ "backgroundColor": "#82cbe5" }}>Title</th>
+                            <th style={{ "backgroundColor": "#82cbe5" }}>Short Description</th>
+                            <th style={{ "backgroundColor": "#82cbe5" }}>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td className='font-semibold'>Should You Invest In Internal...</td>
+                            <td>Aliquam finibus augue ac diam euismod, nec...</td>
+                            <th>
+                                <div className='w-12 h-12 rounded-full bg-red-100 flex justify-center items-center'>
+                                    <div className='text-red-500 text-3xl leading-3'>
+                                        <button className='text-center'><AiFillDelete /></button>
+                                    </div>
+                                </div>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>2</th>
+                            <td className='font-semibold'>After Microsoft Ignite, Keep...</td>
+                            <td>Aliquam finibus augue ac diam euismod, nec...</td>
+                            <th>
+                                <div className='w-12 h-12 rounded-full bg-red-100 flex justify-center items-center'>
+                                    <div className='text-red-500 text-3xl leading-3'>
+                                        <button className='text-center'><AiFillDelete /></button>
+                                    </div>
+                                </div>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>3</th>
+                            <td className='font-semibold'>All English MSDN And TechNet...</td>
+                            <td>Aliquam finibus augue ac diam euismod, nec...</td>
+                            <th>
+                                <div className='w-12 h-12 rounded-full bg-red-100 flex justify-center items-center'>
+                                    <div className='text-red-500 text-3xl leading-3'>
+                                        <button className='text-center'><AiFillDelete /></button>
+                                    </div>
+                                </div>
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
