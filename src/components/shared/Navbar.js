494c7a83c6ed45d5c3e88b5f4e1d1521986a7c48
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-08-20 19:31:00 +0600

    update navbar

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,106 +1,128 @@
-import React, { useState } from 'react';
-import './Navbar.css'
-import { Link } from 'react-router-dom';
-import { IoIosArrowDown } from 'react-icons/io'
-import DarkModeToggle from "react-dark-mode-toggle";
+import React, { useEffect, useState } from 'react';
+import logo from '../../assets/logo/logo4.png'
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Loading from './Loading';
+import { signOut } from 'firebase/auth';
+import blankPic from '../../assets/profile/user-profile.png'
 
-const Navbar = ({ children }) => {
-    const [isDarkMode, setIsDarkMode] = useState(() => false);
-    const myFunction = () => {
-        var element = document.body;
-        element.classList.toggle("dark-mode");
+const Navbar = () => {
+    const [navSize, setnavSize] = useState("6rem");
+    const [navColor, setnavColor] = useState("transparent");
+    const listenScrollEvent = () => {
+        window.scrollY > 10 ? setnavColor("#0FCFEC") : setnavColor("transparent");
+        window.scrollY > 10 ? setnavSize("6rem") : setnavSize("6rem");
+    };
+    useEffect(() => {
+        window.addEventListener("scroll", listenScrollEvent);
+        return () => {
+            window.removeEventListener("scroll", listenScrollEvent);
+        };
+    }, []);
+    const [user, loading] = useAuthState(auth);
+    const navigate = useNavigate()
+    const [userData, setUserData] = useState({});
+    useEffect(() => {
+        const email = user?.email
+        if (email) {
+            fetch(`https://polar-shore-69456.herokuapp.com/user/${email}`, {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json',
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setUserData(data)
+                })
+        }
+    }, [user])
+    const handleSignOut = () => {
+        signOut(auth);
+        localStorage.removeItem('accessToken')
+        navigate('/login')
     }
-
-
+    if (loading) {
+        return <Loading></Loading>
+    }
     return (
-        <div className="drawer drawer-end">
-            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col">
-                <div className="w-full navbar  top-0 fixed  lg:px-10">
-                    <div className="flex-1 px-2 mx-2 text-2xl font-bold"><Link to="/"><span className='text-primary text-3xl font-bold'>E</span>asy<span className='text-secondary text-3xl font-bold'>D</span>ocs</Link></div>
-                    <div className="flex-none lg:hidden">
-                        <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                        </label>
-                    </div>
-                    <div className="flex-none hidden lg:block">
-                        <ul className="menu menu-horizontal">
-                            <div className="menu menu-horizontal mr-[160px]">
-                                <li><Link className='hover:bg-white hover:text-primary' to="/docs">Docs</Link></li>
-                                <li className="dropdown dropdown-hover">
-                                    <label tabIndex="0" className='flex items-center hover:bg-white hover:text-primary'>Blogs<IoIosArrowDown></IoIosArrowDown></label>
-                                    <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                                        <li><Link className='hover:bg-white hover:text-primary' to="/blog">Blog</Link></li>
-                                        <li><Link className='hover:bg-white hover:text-primary' to="/tutorial">Tutorial</Link></li>
-                                    </ul>
-                                </li>
-                                <li><Link className='hover:bg-white hover:text-primary' to="/dashboard">Dashboard</Link></li>
-                                <li className="dropdown dropdown-hover">
-                                    <label tabIndex="0" className='flex items-center hover:bg-white hover:text-primary'>Contact us<IoIosArrowDown></IoIosArrowDown></label>
-                                    <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                                        <li><Link className='hover:bg-white hover:text-primary' to="/contact">Contact Us</Link></li>
-                                        <li><Link className='hover:bg-white hover:text-primary' to="/about">About Us</Link></li>
-                                    </ul>
-                                </li>
-                                <li><Link className='hover:bg-white hover:text-primary' to="/languages">Languages</Link></li>
-                            </div>
-                            {/* <label className="swap swap-rotate mr-[20px]">
-                                <input type="checkbox" />
-                                <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
-                                <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
-
-                            </label> */}
-                            <p onClick={myFunction}>
-                                <DarkModeToggle
-
-                                    className='dark-mode '
-                                    onChange={setIsDarkMode}
-                                    checked={isDarkMode}
-                                    size={70}
-                                />
-                            </p>
-
-                            <li><Link className="btn bg-transparent text-primary btn-primary hover:text-white rounded-full" to="/courses">Our Courses</Link></li>
-                            <li><Link className='btn btn-primary text-white rounded-full ml-4' to="/login">Sign In</Link></li>
-                        </ul>
-                    </div>
-                </div>
-                {children}
-            </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-                <ul className="menu p-4 overflow-y-auto w-80 bg-white">
-                    <div className='text-2xl font-bold ml-4 flex items-center justify-between'>
-                        <Link to="/"><span className='text-primary text-3xl font-bold'>E</span>asy<span className='text-secondary text-3xl font-bold'>D</span>ocs</Link>
-                        <label className="swap swap-rotate mr-4 mt-2">
-                            <input type="checkbox" />
-                            <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
-                            <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
-
-                        </label>
-                    </div>
-                    <li><Link className='hover:bg-white hover:text-primary' to="/docs">Docs</Link></li>
-                    <div className="dropdown ml-4">
-                        <label tabIndex="0" className='flex items-center hover:bg-white hover:text-primary'>Blogs<IoIosArrowDown></IoIosArrowDown></label>
-                        <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link className='hover:bg-white hover:text-primary' to="/blog">Blog</Link></li>
-                            <li><Link className='hover:bg-white hover:text-primary' to="/tutorial">Tutorial</Link></li>
-                        </ul>
-                    </div>
-                    <li><Link className='hover:bg-white hover:text-primary' to="/dashboard">Dashboard</Link></li>
-                    <div className="dropdown ml-4">
-                        <label tabIndex="0" className='flex items-center hover:bg-white hover:text-primary'>Contact us<IoIosArrowDown></IoIosArrowDown></label>
-                        <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link className='hover:bg-white hover:text-primary' to="/contact">Contact Us</Link></li>
-                            <li><Link className='hover:bg-white hover:text-primary' to="/about">About Us</Link></li>
-                        </ul>
-                    </div>
-                    <li><Link className='hover:bg-white hover:text-primary' to="/languages">Languages</Link></li>
-                    <li><Link className="btn bg-transparent text-primary mb-3 ml-4 w-[150px] btn-primary hover:text-white rounded-full" to="/courses">Our Courses</Link></li>
-                    <li><Link className='btn btn-primary w-[150px] text-white rounded-md ml-4' to="/login">Sign In</Link></li>
-                </ul>
-            </div>
-        </div>
+        <div   style={{
+            backgroundColor: navColor,
+            height: navSize,
+            transition: "all 1s"
+        }} class="navbar pt-7">
+  <div class="navbar-start lg:mx-14">
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+      <li><Link className='font-bold text-white' to="/docs">Docs</Link></li>
+    <li><Link className='font-bold' to="/ask">Ask</Link></li>
+    <li><Link className='font-bold' to="/blog">Blog</Link></li>
+    <li><Link className='font-bold' to="/admin-panel">Dashboard</Link></li>
+      <li tabindex="0">
+        <label>
+          Pages
+          <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+        </label>
+        <ul class="p-2 bg-base-100">
+        <li><Link className='font-bold' to="/courses">Cources</Link></li>
+        <li><Link className='font-bold' to="/tutorial">Tutorials</Link></li>
+        <li><Link className='font-bold' to="/contact">Contact Us</Link></li>
+        <li><Link className='font-bold' to="/about">About Us</Link></li>
+        </ul>
+      </li>
+      {
+        user ?  <li tabindex="0">
+        <label className='flex items-center '><img className='w-10 h-10 rounded-full cursor-pointer' src={userData?.photo ? userData?.photo : blankPic} alt="" /></label>
+        <ul class="p-2 bg-gray-100">
+        <li><Link className='font-bold' to="/user-profile">My Profile</Link></li>
+        <li><Link  onClick={handleSignOut} className='font-bold' to="/login">Logout</Link></li>
+        </ul>
+      </li>
+      :
+      <li><Link className='font-bold' to="/login">Login</Link></li>
+      }
+      </ul>
+    </div>
+    <Link to="/"><img className='w-[120px] h-8' src={logo} alt="" /></Link>
+  </div>
+  <div class="navbar-end hidden lg:flex">
+    <ul class="menu menu-horizontal p-0 mr-14">
+    <li><Link className='font-bold text-white' to="/docs">Docs</Link></li>
+    <li><Link className='font-bold' to="/ask">Ask</Link></li>
+    <li><Link className='font-bold' to="/blog">Blog</Link></li>
+    <li><Link className='font-bold' to="/admin-panel">Dashboard</Link></li>
+      <li tabindex="0">
+        <label>
+          Pages
+          <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+        </label>
+        <ul class="p-2 bg-base-100">
+        <li><Link className='font-bold' to="/courses">Cources</Link></li>
+        <li><Link className='font-bold' to="/tutorial">Tutorials</Link></li>
+        <li><Link className='font-bold' to="/contact">Contact Us</Link></li>
+        <li><Link className='font-bold' to="/about">About Us</Link></li>
+        </ul>
+      </li>
+      {
+        user ?  <li tabindex="0">
+        <label className='flex items-center '><img className='w-10 h-10 rounded-full cursor-pointer' src={userData?.photo ? userData?.photo : blankPic} alt="" /></label>
+        <ul class="p-2 bg-gray-100">
+        <li><Link className='font-bold' to="/user-profile">My Profile</Link></li>
+        <li><Link  onClick={handleSignOut} className='font-bold' to="/login">Logout</Link></li>
+        </ul>
+      </li>
+      :
+      <li><Link className='font-bold' to="/login">Login</Link></li>
+      }
+    </ul>
+  </div>
+</div>
     );
 };
+
 export default Navbar;
