1e1759e270c674a534cd0dab0bcbc986c897ff02
unknown <saifulislam1hridoy@gmail.com>
2022-08-31 17:04:03 +0600

    update blog and ask page

---
+++ src/redux/store/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import blogReducer from "../features/blogs/blogSlice";
+import questionReducer from "../features/questions/questionSlice";
+
+const store = configureStore({
+    reducer:{
+        questions:questionReducer,
+        blogs:blogReducer,
+    }
+})
+
+export default store;
