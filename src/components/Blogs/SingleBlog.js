61b92dea27961b1ce1fa493324e449e64fcf1268
unknown <saifulislam1hridoy@gmail.com>
2022-09-04 22:10:10 +0600

    update blog

--- src/components/Blogs/SingleBlog.js
+++ src/components/Blogs/SingleBlog.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import blankPic from "../../assets/profile/user-profile.png";
 import { Link, useNavigate } from 'react-router-dom';
+import {FaRegCommentDots} from 'react-icons/fa'
+import {MdOutlineVisibility} from 'react-icons/md'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const SingleBlog = ({blog}) => {
-    const {blogCategory,blogDescription,blogImage,blogPostDate,blogTitle,blogUploaderName,blogUploaderPhoto,readTime,_id}=blog;
+    const [blogs,setBlogs]=useState([])
+    const {blogCategory,visitor,blogDescription,blogImage,blogPostDate,blogTitle,blogUploaderName,blogUploaderPhoto,readTime,_id}=blog;
+    const handleVisitor=(id)=>{
+    fetch(`https://polar-shore-69456.herokuapp.com/blogVisitor/${id}`,{
+        method:"PUT",
+        headers:{
+         'content-type':"application/json"
+        }
+    })
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+    }
+    useEffect(() => {
+        fetch(`https://polar-shore-69456.herokuapp.com/comment/${_id}`)
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [blogs])
     return (
         <div    
         class="card lg:w-96 md:w-80 w-80 bg-base-100 shadow-md hover:shadow-2xl transition-all">
-        <figure><img src={blogImage} alt="Shoes" className='w-full' /></figure>
+        <figure><img src={blogImage} alt="blog" className='w-[400px] h-[250px]' /></figure>
         <div class="card-body">
             <div className='flex gap-5'>
                 <span className='text-sm' style={{ "color": "#acace6" }}>{readTime} Min Read</span>
                 <span style={{ "color": "#acace6" }}>|</span>
                 <p className='text-blue-500 text-base'>{blogCategory}</p>
             </div>
-            <Link to={`/blogDetails/${_id}`} className='text-2xl hover:text-blue-700 font-medium whiteTextToBlack'>{blogTitle}</Link>
+            <Link onClick={()=>handleVisitor(_id)} to={`/blogDetails/${_id}`} className='text-2xl hover:text-blue-700 font-medium whiteTextToBlack'>{blogTitle}</Link>
             <p className='text-base' style={{ "color": "#6b707f" }}>{blogDescription.slice(0,65)}...</p>
             <div class="card-actions justify-start">
-                <div className='flex gap-5 items-center justify-start mt-8'>
+                <div className='flex gap-5 items-center justify-start mt-4'>
                     <div className='h-12 w-12 rounded-full'>
                         <img src={blogUploaderPhoto?blogUploaderPhoto:blankPic} alt="" className='w-full rounded-full' />
                     </div>
@@ -28,6 +48,10 @@ const SingleBlog = ({blog}) => {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className='flex justify-between'>
+                <p className='flex items-end'>{visitor} <MdOutlineVisibility className='ml-3 w-6 h-6'></MdOutlineVisibility></p>
+                <p className='flex items-center'>{blogs.length} <FaRegCommentDots className='ml-3'></FaRegCommentDots></p>
             </div>
         </div>
     </div>
